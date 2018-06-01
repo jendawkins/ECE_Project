8828201284286b696e38b82a2dc994afd0e1f328
jendawkins <>
2018-06-01 13:26:51 -0700

    first commit

---
+++ episodic_control_jen.py
@@ -0,0 +1,192 @@
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+
+# __author__ = 'sudeep raja'
+import numpy as np
+# import _pickle as cPickle
+import pickle
+import heapq
+# import image_preprocessing as ip
+from sklearn.neighbors import BallTree,KDTree
+
+
+class LRU_KNN:
+    def __init__(self, capacity,dimension_result):
+        self.capacity = capacity
+        self.states = np.zeros((capacity,dimension_result))
+        self.q_values = np.zeros(capacity)
+        self.lru = np.zeros(capacity)
+        self.curr_capacity = 0
+        self.tm = 0.0
+        self.tree = None
+
+    def peek(self,key,value,modify):
+        if self.curr_capacity==0:
+            return None
+
+        # tree = KDTree(self.states[:self.curr_capacity])
+        dist, ind = self.tree.query([key], k=1)
+        ind = ind[0][0]
+
+        if np.allclose(self.states[ind],key):
+            self.lru[ind] = self.tm
+            self.tm +=0.01
+            if modify:
+                self.q_values[ind] = max(self.q_values[ind],value)
+            return self.q_values[ind]
+
+        return None
+
+    def knn_value(self, key, knn=11):
+        if self.curr_capacity==0:
+            return 0.0
+
+        # tree = KDTree(self.states[:self.curr_capacity])
+        # import pdb; pdb.set_trace()
+        dist, ind = self.tree.query([key], k=knn)
+
+        value = 0.0
+        for index in ind[0]:
+            value += self.q_values[index]
+            self.lru[index] = self.tm
+            self.tm+=0.01
+
+        return value / knn
+
+    def add(self, key, value):
+        if self.curr_capacity >= self.capacity:
+            # find the LRU entry
+            old_index = np.argmin(self.lru)
+            self.states[old_index] = key
+            self.q_values[old_index] = value
+            self.lru[old_index] = self.tm
+        else:
+            self.states[self.curr_capacity] = key
+            self.q_values[self.curr_capacity] = value
+            self.lru[self.curr_capacity] = self.tm
+            self.curr_capacity+=1
+        self.tm += 0.01
+        self.tree = KDTree(self.states[:self.curr_capacity])
+
+
+class Node(object):
+    def __init__(self, time, state, q_return):
+        self.lru_time = time  # time stamp used for LRU
+        self.state = state
+        self.QEC_value = q_return
+
+
+class DistanceNode(object):
+    def __init__(self, distance, index):
+        self.distance = distance
+        self.index = index
+
+class QECTable(object):
+    def __init__(self, num_actions, rng, observation_dimension, state_dimension, buffer_size, images):
+        # self.knn = knn
+        self.images = images
+        self.ec_buffer = []
+        self.buffer_maximum_size = buffer_size
+        self.rng = rng
+        for i in range(num_actions):
+            self.ec_buffer.append(LRU_KNN(buffer_size,state_dimension))
+
+        # projection
+        """
+        I tried to make a function self.projection(state)
+        but cPickle can not pickle an object that has an function attribute
+        """
+        if self.images:
+            self._initialize_projection_function(state_dimension, observation_dimension)
+
+    def _initialize_projection_function(self, dimension_result, dimension_observation, p_type='random'):
+        if p_type == 'random':
+            self.matrix_projection = self.rng.randn(dimension_result, dimension_observation).astype(np.float32)
+        elif p_type == 'VAE':
+            pass
+        else:
+            raise ValueError('unrecognized projection type')
+
+    """estimate the value of Q_EC(s,a)  O(N*logK*D)  check existence: O(N) -> KNN: O(D*N*logK)"""
+    def estimate(self, s, a):
+        if self.images:
+            state = np.dot(self.matrix_projection, s.flatten())
+        else:
+            state = s
+            if np.isscalar(state):
+                state = [s]
+        q_value = self.ec_buffer[a].peek(state,None,modify = False)
+        if q_value!=None:
+            return q_value
+        # import pdb; pdb.set_trace()
+        return self.ec_buffer[a].knn_value(state)
+
+    def update(self, s, a, r):  # s is 84*84*3;  a is 0 to num_actions; r is reward
+        if self.images:
+            state = np.dot(self.matrix_projection, s.flatten())
+        else:
+            state = s
+            if np.isscalar(state):
+                state = [s]
+
+        q_value = self.ec_buffer[a].peek(state,r,modify = True)
+        if q_value==None:
+            self.ec_buffer[a].add(state,r)
+
+# Create the CartPole game environment
+env = gym.make('FrozenLake-v0')
+from gym.envs.registration import register
+register(
+    id='FrozenLakeNotSlippery-v0',
+    entry_point='gym.envs.toy_text:FrozenLakeEnv',
+    kwargs={'map_name' : '4x4', 'is_slippery': False},
+    max_episode_steps=100,
+    reward_threshold=0.78, # optimum = .8196
+)
+continuous = False
+# env = gym.make('MountainCar-v0')
+env.reset()
+
+rng = np.random.RandomState(123456)
+obs_dim = 84*84
+action_size = env.action_space.n
+# state_size = env.observation_space.shape[0]
+if continuous:
+    state_dimension = env.observation_space.shape[0]
+    state_size = env.observation_space.shape[0]
+else:
+    state_dimension = 1
+    state_size = env.observation_space.n
+buffer_size = 100000
+ec_discount = .99
+
+qec_table = QECTable(action_size,rng,obs_dim,state_dimension,buffer_size,images=False)
+trace_list = []
+for i in range(50000):
+    state = env.reset()
+    done = False
+    while not done:
+        # maximum_action = np.random.randint(env.action_space.n)
+        # argmax(Q(s,a))
+        value_t = []
+        for action in range(action_size):
+            value_t.append(qec_table.estimate(state, action))
+        maximum_action = np.argmax(value_t)
+        # import pdb; pdb.set_trace()
+            # value = 0
+            # if value_t > value:
+            #     value = value_t
+            #     maximum_action = action
+        state, reward, done , _ = env.step(maximum_action)
+        # if value > 0:
+            # import pdb; pdb.set_trace()
+        trace_list.append((state, maximum_action, reward, done))
+
+    q_return = 0.
+    for i in range(len(trace_list)-1, -1, -1):
+        node = trace_list[i]
+        q_return = q_return * ec_discount + node[2]
+        qec_table.update(node[0], node[1], q_return)
+    if not i % 100:
+        print(q_return)
